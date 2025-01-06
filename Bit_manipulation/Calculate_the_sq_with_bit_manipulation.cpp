6d3e88420469cc590e69b188c66764bf5bed10a6
arshamakhtar <192856576+arshamakhtar@users.noreply.github.com>
2025-01-06 18:45:50 +0530

    Create Calculate_the_sq_with_bit_manipulation.cpp

---
+++ Bit_manipulation/Calculate_the_sq_with_bit_manipulation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int square(int n) {
+    int result = 0;
+    int x = n;
+
+    while (x) {
+        if (x & 1) result += n;
+        n <<= 1;
+        x >>= 1;
+    }
+
+    return result;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout <<"square of the number will be"<< square(n) << endl;
+    return 0;
+}
