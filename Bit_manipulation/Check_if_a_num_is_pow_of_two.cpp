66e3f797e478450e68b3200fc1f0c732d9c96065
arshamakhtar <192856576+arshamakhtar@users.noreply.github.com>
2025-01-06 18:31:21 +0530

    Create Check_if_a_num_is_pow_of_two.cpp

---
+++ Bit_manipulation/Check_if_a_num_is_pow_of_two.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isPowerOfTwo(int n) {
+    return n > 0 && (n & (n - 1)) == 0; 
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPowerOfTwo(num)) {
+        cout << num << " is a power of two." << endl;
+    } else {
+        cout << num << " is not a power of two." << endl;
+    }
+
+    return 0;
+}
