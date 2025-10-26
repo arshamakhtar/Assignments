f621070b79eb4d9c7c493fab96cc55943fa2c0bc
arshamakhtar <arshamakhtar123@gmail.com>
2025-10-26 22:15:08 +0530

    15th october

--- Swayam/17th_October/heightOfTree.cpp
+++ Swayam/17th_October/heightOfTree.cpp
@@ -1,35 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-//build tree
-struct TreeNode {
+// build tree
+struct TreeNode
+{
     int val;
-    TreeNode* left;
-    TreeNode* right;
+    TreeNode *left;
+    TreeNode *right;
 
-    TreeNode(int v) {
+    TreeNode(int v)
+    {
         val = v;
         left = right = nullptr;
     }
 };
 
-
-int height(TreeNode* root) {
-    if (!root) return 0;
+int height(TreeNode *root)
+{
+    if (!root)//exit at the leafnode
+        return 0;
     return 1 + max(height(root->left), height(root->right));
 }
 
-int main(){
+int main()
+{
 
-    TreeNode* node= new TreeNode(1);
-    node -> right= new TreeNode(2);
-    node -> left = new TreeNode(3);
-    node -> left -> left = new TreeNode(6);
-    node -> right->right = new TreeNode(4);
-    node-> left -> left->right = new TreeNode(5);
-    node->left -> right = new TreeNode(6);
+    TreeNode *node = new TreeNode(1);
+    node->right = new TreeNode(2);
+    node->left = new TreeNode(3);
+    node->left->left = new TreeNode(6);
+    node->right->right = new TreeNode(4);
+    node->left->left->right = new TreeNode(5);
+    node->left->right = new TreeNode(6);
 
-    cout<<"Height of tree: "<<endl;
-    cout<<height(node)<<endl;
+    cout << "Height of tree: " << endl;
+    cout << height(node) << endl;
     return 0;
 }
